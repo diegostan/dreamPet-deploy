afaba8de1093dd325628cb1d15325d215d990eda
diegostan <diego150691@gmail.com>
2021-05-10 13:27:16 -0300

    First Azure Deploy

--- Startup.cs
+++ Startup.cs
@@ -49,7 +49,7 @@ namespace DPBack.Domain.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (!env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
